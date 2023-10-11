e4b3f36f7b782182431ce1adc2d764236cbccd82
Stephen Nnamani <steph-nnamani@gmail.com>
2023-10-10 20:56:57 -0500

    fix: bugs

--- release.config.js
+++ release.config.js
@@ -4,7 +4,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/git",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
     "@semantic-release/github"
   ]
 };
+
