52eb4bdc5ba28106e63a72d575bcba514b535e51
Stephen Nnamani <steph-nnamani@gmail.com>
2023-10-10 20:15:23 -0500

    fix: bugs

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-branches: "main"
-repositoryUrl: "https://github.com/steph-nnamani/s3-remote-backend.git/s3-module"
-plugins: [
-'@semantic-release/commit-analyzer',
-'@semantic-release/release-notes-generator',
-'@semantic-release/git',
-'@semantic-release/github']
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/steph-nnamani/s3-remote-backend.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/git",
+    "@semantic-release/github"
+  ]
+};
